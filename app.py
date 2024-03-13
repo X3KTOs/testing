06d14db223c9587dcbda50a73191e1d4923436a1
X3KTOs <162344922+X3KTOs@users.noreply.github.com>
2024-03-13 08:19:53 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py 
+from flask import Flask 
+
+app = Flask(__name__) 
+
+@app.route('/', methods=['GET']) 
+def home(): 
+	return "Homepage"
+@app.route('/contact', methods=['GET']) 
+def contact(): 
+	return "Contact page" 
+if __name__ == "__main__": 
+	app.run()
